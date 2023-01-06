2d5aa810567a990df96d3a3acff125921474f18b
Yeuri07 <Trabajo_utesad@hotmail.com>
2023-01-06 11:41:47 -0400

    Add promos

--- script/lenguaje.js
+++ script/lenguaje.js
@@ -9,3 +9,14 @@ function lenguaje() {
     window.location.assign("index.html");
   }
 }
+
+
+var imagenes = ['/img/promo1.jpg', '/img/promo2.jpg', '/img/promo3.jpg'];
+var contador = 0;
+
+function cambiarImagen() {
+  document.getElementById("promo1").src = imagenes[contador];
+  contador = (contador + 1) % imagenes.length;
+}
+
+setInterval(cambiarImagen, 2000);
